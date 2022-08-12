920d84bc4d6d57176304407ef6055166abbc119e
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2022-08-12 12:23:23 -0700

    Create MapDemoCarCounter.java

---
+++ MapDemoCarCounter.java
@@ -0,0 +1,19 @@
+package mypack2;
+
+//count cars by color
+
+public class MapDemoCarCounter
+  {
+  public static void main(String[] arg)
+    {
+     Car c1 = new Car();
+     Car c2 = new Car();
+    }
+  }
+  
+class Car
+  {
+  int id;
+  String name;
+  String color;
+  }
