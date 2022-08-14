8914ed727afd59edf8f1d744b37edfa197ac1943
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2022-08-13 21:15:00 -0700

    Update MapDemoCarCounter.java

--- MapDemoCarCounter.java
+++ MapDemoCarCounter.java
@@ -6,14 +6,22 @@ public class MapDemoCarCounter
   {
   public static void main(String[] arg)
     {
-     Car c1 = new Car();
-     Car c2 = new Car();
+     Car c1 = new Car(); //Object type is Car & object is c1
+     Car c2 = new Car(); //Object type is Car & object is c2
     }
   }
   
 class Car
   {
-  int id;
-  String name;
-  String color;
+  int id; //car manufacturing identification
+  String name; //car name
+  String color; //car color
+  
+  public Car(int id, String name, String color) //creating constructor for car
+    {
+    super();
+    this.id = id;
+    this.name = name;
+    this.color = color;
+    }
   }
