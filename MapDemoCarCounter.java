94ab0b9c0a21bfbeea91ae7efe040edaf0256490
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2022-08-15 02:16:38 -0700

    Update MapDemoCarCounter.java

--- MapDemoCarCounter.java
+++ MapDemoCarCounter.java
@@ -5,14 +5,8 @@ package mypack2;
 public class MapDemoCarCounter
   {
   public static void main(String[] args)
-    {
-    //below are two constructors
-     Car c1 = new Car(); //Object type is Car & object is c1
-      c1.id = 20;
-      c1.name = "tesla";
-      c1.color = "blue";
-    
-     Car c2 = new Car(1, "hyundai", "red"); //Object type is Car & object is c2, id is 1, car name is hyundai, color is red
+    {    
+     Car c1 = new Car(1, "hyundai", "red"); //Object type is Car & object is c2, id is 1, car name is hyundai, color is red
     }
   }
   
@@ -40,12 +34,18 @@ class Car extends Machine //for the car class, machine is the super-class
     }
   }
 
-class Machine
+class Machine extends Object
 {
 int machineId;
 String manufacturingName;
   public Machine()
     {
+    super();
     System.out.println("inside default constructor of Machine");
     }
 }
+
+
+//Interview questions that can be asked. 
+//What's constructor? What's constructor over-loading? What's constructor chaining?
+//What is super()?
